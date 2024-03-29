5abfba83b0bf11b92bf371e1ca9ae779b3a1f7e1
_tango <shurinskiy@mail.ru>
2024-03-30 00:23:48 +0500

    hero work

--- src/blocks/hero/hero.js
+++ src/blocks/hero/hero.js
@@ -1,4 +1,23 @@
+import Swiper from 'swiper';
+import { Autoplay, Pagination } from 'swiper/modules';
+
 (() => {
 
+	new Swiper(".hero__slider", {
+		modules: [ Autoplay, Pagination ],
+		spaceBetween: 0,
+		loop: true,
+		speed: 1000,
+		autoplay: { 
+			delay: 10000,
+			disableOnInteraction: true
+		},
+		pagination: {
+			el: '.hero__pagination',
+			bulletClass: 'hero__dot',
+			bulletActiveClass: 'active',
+			clickable: true,
+		}
+	});
 
 })();
